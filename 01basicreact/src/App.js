e01c59ad2e2d1f30ce64c2db2861a4139ad2aa90
Chee Wei Liang <cheeweiliang8@gmail.com>
2024-03-24 20:53:41 +0800

    Basic React and Basic Vite Working Starting Template

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>classic react app</h1>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return (<h1>vite react app</h1>);
+}
+
+export default App;
