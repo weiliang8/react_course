1859769a1bb0932c5e137977566eb94db237b8e3
Chee Wei Liang <cheeweiliang8@gmail.com>
2024-03-27 20:50:44 +0800

    Props and tailwindCSS configuration Part 1 UNFINISHED

---
+++ 03tailwindprops/tailwind.config.js
@@ -0,0 +1,10 @@
+// TO SOLVE TAILWINDCSS NOT RESPONDING CONTENT CHANGE EXTENSION JS TO JSX
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
